21ea4988cf85fe1dbbf5271686f26109a2ba7ecf
ne1410s <pgj646@gmail.com>
2025-02-23 16:24:12 +0000

    couple cache tests

---
+++ source/EnterpriseStartup.Tests/Caching/MemoryCacheTests.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="MemoryCacheTests.cs" company="ne1410s">
+// Copyright (c) ne1410s. All rights reserved.
+// </copyright>
+
+namespace EnterpriseStartup.Tests.Caching;
+
+using EnterpriseStartup.Caching;
+using Microsoft.Extensions.Logging;
+
+/// <summary>
+/// Tests for the <see cref="MemoryCache"/> class.
+/// </summary>
+public class MemoryCacheTests
+{
+    [Fact]
+    public async Task GetValue_NullFactory_ThrowsException()
+    {
+        // Arrange
+        var sut = GetSut(out _);
+
+        // Act
+        var act = () => sut.GetValue<string>(string.Empty, null!);
+
+        // Assert
+        (await act.ShouldThrowAsync<ArgumentNullException>())
+            .ParamName.ShouldBe("factory");
+    }
+
+    private static MemoryCache GetSut(out Mock<ILogger<MemoryCache>> mockLogger)
+    {
+        mockLogger = new Mock<ILogger<MemoryCache>>();
+        return new MemoryCache(mockLogger.Object);
+    }
+}
---
+++ source/EnterpriseStartup.Tests/Caching/RedisCacheTests.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="RedisCacheTests.cs" company="ne1410s">
+// Copyright (c) ne1410s. All rights reserved.
+// </copyright>
+
+namespace EnterpriseStartup.Tests.Caching;
+
+using EnterpriseStartup.Caching;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+/// <summary>
+/// Tests for the <see cref="RedisCache"/> class.
+/// </summary>
+public class RedisCacheTests
+{
+    [Fact]
+    public async Task GetValue_NullFactory_ThrowsException()
+    {
+        // Arrange
+        var sut = GetSut(out _, out _);
+
+        // Act
+        var act = () => sut.GetValue<string>(string.Empty, null!);
+
+        // Assert
+        (await act.ShouldThrowAsync<ArgumentNullException>())
+            .ParamName.ShouldBe("factory");
+    }
+
+    private static RedisCache GetSut(
+        out Mock<ILogger<RedisCache>> mockLogger,
+        out Mock<IDatabase> mockRedis)
+    {
+        mockLogger = new Mock<ILogger<RedisCache>>();
+        mockRedis = new Mock<IDatabase>();
+        return new RedisCache(mockLogger.Object, mockRedis.Object);
+    }
+}
