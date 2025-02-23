8812a3d1f0671510b8fe3b3efc0dda0603621e3e
ne1410s <pgj646@gmail.com>
2025-02-23 18:50:54 +0000

    add maybe final vanilla test

--- source/EnterpriseStartup.Tests/Caching/RedisCacheTests.cs
+++ source/EnterpriseStartup.Tests/Caching/RedisCacheTests.cs
@@ -27,6 +27,80 @@ public class RedisCacheTests
             .ParamName.ShouldBe("factory");
     }
 
+    [Fact]
+    public async Task GetValue_DoesNotExist_FactoryCalled()
+    {
+        // Arrange
+        var sut = GetSut(out _, out _);
+        var expected = Guid.NewGuid();
+
+        // Act
+        var actual = await sut.GetValue("myKey", () => Task.FromResult(expected));
+
+        // Assert
+        actual.ShouldBe(expected);
+    }
+
+    [Fact]
+    public async Task GetValue_AlreadyExists_UsesCache()
+    {
+        // Arrange
+        var sut = GetSut(out _, out _);
+        var expected = Guid.NewGuid();
+        await sut.SetDirectly("myKey", expected);
+
+        // Act
+        var actual = await sut.GetValue("myKey", () => Task.FromResult(Guid.Empty));
+
+        // Assert
+        actual.ShouldBe(expected);
+    }
+
+    [Fact]
+    public async Task GetValue_ExistsButExpired_UsesFactory()
+    {
+        // Arrange
+        var sut = GetSut(out _, out _);
+        var expected = Guid.NewGuid();
+        await sut.SetDirectly("myKey", Guid.Empty, TimeSpan.Zero);
+        await Task.Delay(50);
+
+        // Act
+        var actual = await sut.GetValue("myKey", () => Task.FromResult(expected));
+
+        // Assert
+        actual.ShouldBe(expected);
+    }
+
+    [Fact]
+    public async Task GetDirectly_NotFound_ReturnsDefault()
+    {
+        // Arrange
+        var sut = GetSut(out _, out _);
+        const int expected = 0;
+
+        // Act
+        var actual = await sut.GetDirectly<int>("myKey");
+
+        // Assert
+        actual.ShouldBe(expected);
+    }
+
+    [Fact]
+    public async Task GetDirectly_IsFound_ReturnsValue()
+    {
+        // Arrange
+        var sut = GetSut(out _, out _);
+        const int expected = 42;
+        await sut.SetDirectly("myKey", expected);
+
+        // Act
+        var actual = await sut.GetDirectly<int>("myKey");
+
+        // Assert
+        actual.ShouldBe(expected);
+    }
+
     private static RedisCache GetSut(
         out Mock<ILogger<RedisCache>> mockLogger,
         out Mock<IDatabase> mockRedis)
